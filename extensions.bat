d9efa2c32af701a4072221832f69360d1e533682
Rachna <rachnasinghal2005@gmail.com>
2019-01-13 16:50:14 +0530

    Initial commit

---
+++ extensions.bat
@@ -0,0 +1,43 @@
+echo.
+echo.
+echo Installing VS Code Extensions...
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension RoscoP.ActiveFileInStatusBar
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension akamud.vscode-theme-onedark
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension akamud.vscode-theme-onelight
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension asvetliakov.snapshot-tools
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension ban.spellright
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension christian-kohler.npm-intellisense
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension christian-kohler.path-intellisense
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension cmstead.jsrefactor
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension CoenraadS.bracket-pair-colorizer
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension dbaeumer.vscode-eslint
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension deerawan.vscode-dash
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension EditorConfig.EditorConfig
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension eg2.tslint
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension eg2.vscode-npm-script
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension emmanuelbeziat.vscode-great-icons
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension ericadamski.carbon-now-sh
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension esbenp.prettier-vscode
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension formulahendry.auto-rename-tag
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension gcazaciuc.vscode-flow-ide
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension ionutvmi.path-autocomplete
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension jakob101.RelativePath
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension jasonnutter.search-node-modules
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension leizongmin.node-module-intellisense
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension mgmcdermott.vscode-language-babel
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension mrmlnc.vscode-duplicate
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension ms-vscode.atom-keybindings
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension paulmolluzzo.convert-css-in-js
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension PKief.material-icon-theme
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension pnp.polacode
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension sdras.night-owl
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension Shan.code-settings-sync
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension silvenon.mdx
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension sleistner.vscode-fileutils
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension WallabyJs.quokka-vscode
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension wayou.vscode-todo-highlight
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension wmaurer.join-lines
+call "C:\Program Files\Microsoft VS Code\bin\code" --force --install-extension xyz.local-history
+echo Done.
+echo. 
+echo.
